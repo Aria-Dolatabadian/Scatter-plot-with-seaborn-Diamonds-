3108d04b705d326488b0f7b33ab2825d1be2c37f
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-06 12:03:52 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+df = pd.read_csv (r'diamonds.csv')
+print (df)
+import seaborn as sns
+import matplotlib.pyplot as plt
+f, ax = plt.subplots(figsize=(6.5, 6.5))
+sns.despine(f, left=True, bottom=True)
+clarity_ranking = ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"]
+sns.scatterplot(x="carat", y="price",
+                hue="clarity", size="depth",
+                palette="ch:r=-.2,d=.3_r",
+                hue_order=clarity_ranking,
+                sizes=(1, 8), linewidth=0,
+                data=df, ax=ax)
+plt.grid(True)
+plt.show()
